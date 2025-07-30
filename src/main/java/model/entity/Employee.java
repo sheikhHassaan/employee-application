fd602a46efa15188a3afa4a708e21de4eede9c28
hassaan-basil <hassaan.basil@ascertia.com>
2025-07-30 16:22:25 +0500

    ORM Entity correction.

--- src/main/java/model/entity/Employee.java
+++ src/main/java/model/entity/Employee.java
@@ -1,36 +1,46 @@
 package model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
-@Entity(name = "employee")
+@Entity
 @Table(name = "employees")
 public class Employee {
 
     @Id
+    @Column(name = "id")
     private String id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "address")
     private String address;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "phone")
     private String phoneNo;
 
+    @Column(name = "department")
     private String department;
 
+    @Column(name = "designation")
     private String designation;
 
-    private String reportsTo;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "reports_to")
+    private Employee reportsTo;
 
+    @Column(name = "salary")
     private double salary;
 
+    @Column(name = "experience")
     private double experience;
 
 }
